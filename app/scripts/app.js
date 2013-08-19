694c1d54e433255a093df012eebbdd9df2364a63
Francesco Fienga <f.fienga@gmail.com>
2013-08-19 10:02:56 +0200

    Refactoring Code to upgrade Angular Version to 1.2.0rc1

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('AngularPlaygroundApp', [])
+angular.module('AngularPlaygroundApp', ['ngRoute'])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
